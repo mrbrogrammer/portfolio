13105dafd4bd30e2d9723bdebcc232d933ab7dcc
simphiwemadi <seesimbarun@gmail.com>
2025-09-08 22:12:45 +0200

    WIP

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,7 +4,7 @@ $(function() {
 	workLoad();
 	clientStuff();
 
-	setInterval($('.client-control-next').click(), 5000);
+	setInterval(function() {$('.client-control-next').click()}, 5000);
 
 	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
 	$(".biglink").fitText(1.5);
