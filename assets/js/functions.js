c94871d8dc593279d305765bce0d65639c8254b0
simphiwemadi <seesimbarun@gmail.com>
2025-10-20 17:49:59 +0200

    Added parallax to youtube strip

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -23,6 +23,16 @@ $(function() {
 });
 
 $(window).scroll(function(){
+	youtubeVidScroll();
+});
+
+function youtubeVidScroll(){
+	var wScroll = $(window).scrollTop();
+
+$('.video-strip').css('background-position','center -'+ wScroll + 'px');
+}
+
+$(window).scroll(function(){
 	startBlog();
 	startClients();
 });
