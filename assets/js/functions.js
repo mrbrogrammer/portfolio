c5e738bf49c12289a7a8d041b1fab9f4e42c1603
simphiwemadi <seesimbarun@gmail.com>
2025-10-26 11:18:34 +0200

    Implemented blog anchor scroll

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -137,7 +137,7 @@ function blogTada() {
 function startBlog() {
 	var wScroll = $(window).scrollTop();
 
-	if ($('.section-blog').offset().top - $(window).height() / 2 < wScroll) {
+	if ($('#blog').offset().top - $(window).height() / 2 < wScroll) {
 		$('.article-thumb').each(function(i) {
 			setTimeout(function(){$('.article-thumb').eq(i).addClass('is-visible')}, 200 * i);
 		});
